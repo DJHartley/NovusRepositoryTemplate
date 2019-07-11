814334fbcb4bf41bb9e114df08cf656fb4be76c1
Jon Doe <john@bugmenot.com>
2019-07-11 09:01:47 -0500

    Trying to fix inrelease problem

--- update.sh
+++ update.sh
@@ -13,67 +13,70 @@
 #See the License for the specific language governing permissions and  
 #limitations under the License. 
 
- script_full_path=$(dirname "$0") 
-DATE=$(date -R -u)  
-cd $script_full_path  
+#!/bin/bash
+script_full_path=$(dirname "$0")
+DATE=$(date -R -u)
+cd $script_full_path
 
- rm ./Packages.xz ./Packages.gz ./Release.gpg ./Release 
+rm Packages Packages.xz Packages.gz Release.gpg Release
 
- dpkg-scanpackages -m . > ./Packages  
-gzip -c9 -k ./Packages > ./Packages.gz  
-xz -c9 -k ./Packages > ./Packages.xz  
+dpkg-scanpackages -m . > Packages
+gzip -c9 -k Packages > Packages.gz
+xz -c9 -k Packages > Packages.xz
 
- OUTPUT="Release" 
-FILES=( 
-  ./Packages  
-  ./Packages.gz 
-  ./Packages.xz 
-  ./Release 
-) 
+cd $script_full_path
 
- RETURN=false 
+OUTPUT="Release"
+FILES=(
+  Packages
+  Packages.gz
+  Packages.xz
+  Release
+)
+
+RETURN=false
 
 
- function generateHash() {  
-    local CHECKSUM=$($1 $2 | awk '{print $1}')  
-    local SIZE=$(wc -c < $2)  
-    RETURN="${CHECKSUM} ${SIZE} $2" 
-} 
+function generateHash() {
+    local CHECKSUM=$($1 $2 | awk '{print $1}')
+    local SIZE=$(wc -c < $2)
+    RETURN="${CHECKSUM} ${SIZE} $2"
+}
 
- echo 'Origin: yourRepoNameHere 
-Label: yourRepoNameHere 
-Suite: stable 
-Version: 1.0  
-Codename: macOS 
-Date: '$DATE' 
-Architectures: darwin-amd64 
-Components: main  
-Description: Your amazing description about your repository here' >> ${OUTPUT}  
+echo 'Origin: TestRepo
+Label: TestRepo
+Suite: stable
+Version: 1.0
+Codename: macOS
+Date: '$DATE'
+Architectures: darwin-amd64
+Components: main
+Description: Testrepo 1.0' >> ${OUTPUT}
 
- echo "MD5Sum:" >> ${OUTPUT}  
-for i in "${FILES[@]}"; do  
-    generateHash md5sum ${i}  
-    echo " ${RETURN}" >> ${OUTPUT}  
-done  
+echo "MD5Sum:" >> ${OUTPUT}
+for i in "${FILES[@]}"; do
+    generateHash md5sum ${i}
+    echo " ${RETURN}" >> ${OUTPUT}
+done
 
- echo "SHA1:" >> ${OUTPUT}  
-for i in "${FILES[@]}"; do  
-    generateHash sha1sum ${i} 
-    echo " ${RETURN}" >> ${OUTPUT}  
-done  
+echo "SHA1:" >> ${OUTPUT}
+for i in "${FILES[@]}"; do
+    generateHash sha1sum ${i}
+    echo " ${RETURN}" >> ${OUTPUT}
+done
 
- echo "SHA256:" >> ${OUTPUT}  
-for i in "${FILES[@]}"; do  
-    generateHash sha256sum ${i} 
-    echo " ${RETURN}" >> ${OUTPUT}  
-done  
+echo "SHA256:" >> ${OUTPUT}
+for i in "${FILES[@]}"; do
+    generateHash sha256sum ${i}
+    echo " ${RETURN}" >> ${OUTPUT}
+done
 
- echo "SHA512:" >> ${OUTPUT}  
-for i in "${FILES[@]}"; do  
-generateHash sha512sum ${i} 
-echo " ${RETURN}" >> ${OUTPUT}  
-done  
+echo "SHA512:" >> ${OUTPUT}
+for i in "${FILES[@]}"; do
+generateHash sha512sum ${i}
+echo " ${RETURN}" >> ${OUTPUT}
+done
 
- gpg -abs --default-key F0F27C8758ADE7983CA32739EE6CD017B9244FB1 -o ./Release.gpg ./Release #Change your key here!  
+gpg -abs --default-key F0F27C8758ADE7983CA32739EE6CD017B9244FB1 -o ./Release.gpg ./Release
 
- echo "Done updating!"
+echo "Done updating!"
